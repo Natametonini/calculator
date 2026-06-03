0facce5e8a78f87e7e9d37f93d51a5fee5216d28
Natametonini <118022758+Natametonini@users.noreply.github.com>
2026-06-02 23:46:33 -0300

    Feat: Working calculator

--- script.js
+++ script.js
@@ -1,52 +1,182 @@
-let valueOne;
-let valueTwo;
-let operator;
+// VARIABLES -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-function add(numberOne, numberTwo) {
-    return numberOne + numberTwo;
-}
+// Display variables 
+let valueOne = "";
+let valueTwo = "";
+let operator = "";
 
-function subtract(numberOne, numberTwo) {
-    return numberOne - numberTwo;
-}
-
-function multiply(numberOne, numberTwo) {
-    return numberOne * numberTwo;
-}
-
-function divide(numberOne, numberTwo) {
-    return numberOne / numberTwo;
-}
-
-function operate(firstNumber, operator, secondNumber) {
-    switch (operator) {
-        case "+":
-            add(firstNumber, secondNumber);
-            break;
-        case "-":
-            subtract(firstNumber, secondNumber);
-            break;
-        case "*":
-            multiply(firstNumber, secondNumber);
-            break;
-        case "/":
-            divide(firstNumber, secondNumber);
-            break;
-    } 
-}
-
+// Element variables 
+let display = document.getElementById("display");
 let numberBtns = document.querySelector(".numberOptions");
+let operatorBtns = document.querySelector(".operatorOptions");
+let clearBtn = document.querySelector(".clearBtn");
+let equalBtn = document.querySelector(".equal");
+let dotBtn = document.querySelector(".dotBtn");
 
-    numberBtns.addEventListener("click", (e) => {
-        let display = document.getElementById("display");
-        if (valueOne === undefined) { 
-            valueOne = parseInt(e.target.textContent); 
-            display.innerHTML(valueOne)
-        }
-        else { 
-            valueTwo = parseInt(e.target.textContent); 
+// FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Operations Functions 
+function add(firstValue, secondValue) {
+    valueOne = firstValue + secondValue;
+    return valueOne;
+
+}
+
+function subtract(firstValue, secondValue) {
+
+    valueOne = firstValue - secondValue;
+    return valueOne;
+
+}
+
+function multiply(firstValue, secondValue) {
+
+    valueOne = firstValue * secondValue;
+    return valueOne;
+
+}
+
+function divide(firstValue, secondValue) { 
+
+    valueOne = firstValue / secondValue;
+    return valueOne;
+
+}
+
+// Main function 
+function operate(firstValue, operator, secondValue) {
+
+    switch (operator) {
+
+        case "+":
+            add(firstValue, secondValue);
+            break;
+
+        case "-":
+            subtract(firstValue, secondValue);
+            break;
+
+        case "*":
+            multiply(firstValue, secondValue);
+            break;
+
+        case "/":
+            divide(firstValue, secondValue);
+            break;  
+
+    }
+
+}
+    
+// EVENTS -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Add numbers
+numberBtns.addEventListener("click", (event) => {
+
+    if (operator === "") {
+
+        if (event.target.localName === "button") {
+    
+            valueOne += 
+            event.target.textContent;
+
         }
         
-    })  
+        display.textContent = 
+        `${valueOne} ${operator} ${valueTwo}`;
 
-let temporaryText = valueOne;
+    }
+
+    else if (operator !== "") {
+        
+        valueOne = parseFloat(valueOne);
+
+        if (event.target.localName === "button") {
+            
+            valueTwo += 
+            event.target.textContent;
+
+        }
+
+        display.textContent = 
+        `${valueOne} ${operator} ${valueTwo}`;
+
+    }
+
+});
+
+// Select operator
+operatorBtns.addEventListener("click", (event) => {
+
+    dotBtn.disabled = false;
+
+    if (event.target.className === "operator") {
+        if (valueOne !== "") operator = event.target.textContent;
+    }
+
+    display.textContent = 
+    `${valueOne} ${operator} ${valueTwo}`;  
+
+})
+
+// Clear all the numbers
+clearBtn.addEventListener("click", () => {
+    
+    dotBtn.disabled = false;
+    display.textContent = "0";
+    valueOne = "";
+    valueTwo = "";
+    operator = "";
+
+
+})  
+
+// Display the result
+equalBtn.addEventListener("click", () => {
+
+    valueTwo = parseFloat(valueTwo);
+
+    if (operator === "/" && valueTwo === 0) {
+
+        display.textContent = 
+        "Cant divide by 0";
+
+        valueOne = "";
+        valueTwo = "";
+        operator = "";
+
+        return;
+
+    }
+    else {
+
+        operate(valueOne, operator, valueTwo);
+        valueTwo = "";
+        operator = "";
+
+        display.textContent = 
+        `${valueOne} ${operator} ${valueTwo}`;
+
+    }
+    
+}); 
+
+// Make decimal
+dotBtn.addEventListener("click", () => {
+    dotBtn.disabled = true;
+})
+
+// Delete the last digit 
+document.addEventListener("keydown", (event) => {
+
+    if (event.key === "Backspace") {
+
+        if (operator === "") valueOne = valueOne.slice(0, -1);
+        else valueTwo = valueTwo.slice(0, -1);
+
+        display.textContent = 
+        `${valueOne} ${operator} ${valueTwo}`;
+
+    }
+    
+})
